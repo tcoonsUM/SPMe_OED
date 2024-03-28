7d014626021badfaf9ad93b9d1d1658835881ea5
siddhantmich <123673064+siddhantmich@users.noreply.github.com>
2024-03-28 15:19:40 -0400

    Changed diffusivity function to be compatible with pybamm c_s_p

--- run_code.py
+++ run_code.py
@@ -51,7 +51,8 @@ def run_battery_simulation(a_nmc, b_nmc, c_nmc, d_nmc, my_graphite_diff_paramete
 
         #print("Exponent:", exp(a*c_s_p**3+b*c_s_p**2+c0*c_s_p+d))
         #return 8e-15
-        reg_output = diff_model(c_s_p,np.array([d,c0,b,a]))
+        #reg_output = diff_model(c_s_p,np.array([d,c0,b,a]))
+        reg_output = exp( 1*d + 2*c_s_p*c0 + ( 4*c_s_p**2 - 2 ) * b + ( 8*c_s_p**3 - 12*c_s_p)*a )
         return minimum(maximum(reg_output,3e-15),2.5e-13) #max and min values from Chueh paper, fitting is also from Chueh paper
 
 
