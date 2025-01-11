OED script using BoTorch, small fixes to multiplicative noise underflow
Use safe_prod and some numerical tricks to prevent numerical underflow when g<<1

OED script using BoTorch and another cool trick for optimizing without nonlinear constraints (absorbing nonlinearity of constraints into the objective function)

OED/n_nmc_test.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     corrs_clustered_npz = np.load("corrs_clustered_list.npz")
     clusters_inds_npz = np.load("clusters_list.npz") # can access keywords via clusters_npz.files    
     rel_std = np.load("relative_stds.npy")
-    cov_all = cov_all4#np.load("cov_all_1e-5.npy")
+    cov_all = np.load("cov_1e-6.npy")#-np.diag(np.ones(135)*1e-4)+np.diag(np.ones(135)*1e-7)#"ov_all_1e-5.npy")#+np.diag(np.ones(135)*1e-4)
     
     # for testing only
     y_test = np.load("y_cleaned.npy")
@@ -44,9 +44,9 @@ if __name__ == '__main__':
     lb = np.array([0.1, 36.  , 100., 0.1,  1.,  10., 0.001, 25.  ])
     ub = np.array([2. , 3600., 1800, 2. , 20., 600., 0.01 , 1000.])
     
-    d_test = ((lb+ub)/2).reshape(1,-1)
-    d_test[0,1] = d_test[0,0]*120 # d1 must be in [72/d0, 360/d0]
-    d_test[0,7] = d_test[0,6]*0.67 # d7 must be in [.25/d6, 1/d6]
+    d_test = ((lb+ub)/1.5).reshape(1,-1)
+    d_test[0,1] = 200/d_test[0,0]#80#200 # d1 must be in [72/d0, 360/d0]
+    d_test[0,7] = .9/d_test[0,6]#.3#0.9 # d7 must be in [.25/d6, 1/d6]
     #%
     # n_test=100
     # thetas_test = torch.tensor(ute.sample_prior(n_test).T)
@@ -57,20 +57,19 @@ if __name__ == '__main__':
 
     # uncomment to perform N_nmc pilot study
     
-    num_tests = 21
+    num_tests = 31
     utilities_list = []
     uds = np.zeros((num_tests,))
     ud_vars = np.zeros((num_tests,))
     durs = np.zeros((num_tests,))
-    n_tests = np.linspace(100,8100,num_tests)#np.logspace(2, 3, num_tests)#np.linspace(100,1000,num_tests)
-    jitter=1e-3
+    n_tests = np.linspace(100,2100,num_tests)#np.logspace(2, 3, num_tests)#np.linspace(100,1000,num_tests)
     i=0
     for n_test in n_tests:
         n_test_int = int(n_test)
         print("Now running for n_test: "+str(n_test_int))
         start_time = time.time()
         #utilities = ute.eig_mp(d_test, n_test_int, n_test_int, rel_std, clusters_inds_npz, corrs_clustered_npz, jitter=1e-3)
-        utilities = ute.eig_mp_cov(d_test, n_test_int, n_test_int, cov_all, jitter=jitter)
+        utilities = ute.eig_mp_cov(d_test, n_test_int, n_test_int, cov_all, seed=2)
         stop_time = time.time()
         dur = stop_time-start_time
         print("EIG = "+str(utilities.mean()))
@@ -81,8 +80,22 @@ if __name__ == '__main__':
         durs[i] = dur
         i+=1
         
+    plt.plot(n_tests, uds); plt.xlabel(r'$N$'); plt.ylabel(r'$U(d)$'); plt.title('NMC EIG Convergence')
 
-
+    #%%
+    uds = [np.load("ud_cov1e-4.npy"), np.load("ud_cov1e-5.npy"),
+           np.load("ud_cov1e-6.npy"), np.load("ud_cov1e-7.npy"),
+           np.load("ud_cov1e-8.npy")]
+    
+    labels = ["1e-4", "1e-5", "1e-6", "1e-7", "1e-8"]
+    
+    fig, ax = plt.subplots()
+    for i in range(len(uds)):
+        ax.plot(n_tests, uds[i], label=labels[i])
+    ax.legend()
+    ax.set_title("NMC convergence for various jitter magnitudes")
+    ax.set_xlabel(r'$N$')
+    ax.set_ylabel(r'$\hat{U}_{N}(d)$')
     # #%% test functions in utils
     
     # import time

OED/oed_script.py
@@ -10,7 +10,7 @@ import numpy as np
 from scipy import stats
 import os
 import matplotlib.pyplot as plt
-import utils as ute
+import utils_oed as ute
 import torch
 import g
 import time
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     y_test = np.load("y_cleaned.npy")
     
     #% define OED parameters
-    n_in = 1e4
-    n_out = 1e4
+    n_in = int(1e2)
+    n_out = int(1e2)
     n_theta = 8
     n_y = 135
     
@@ -53,8 +53,8 @@ if __name__ == '__main__':
     ub = np.array([2. , 3600., 1800, 2. , 20., 600., 0.01 , 1000.])
     
     d_test = ((lb+ub)/2).reshape(1,-1)
-    d_test[0,1] = d_test[0,0]*120 # d1 must be in [72/d0, 360/d0]
-    d_test[0,7] = d_test[0,6]*0.67 # d7 must be in [.25/d6, 1/d6]
+    d_test[0,1] = 120/d_test[0,0] # d1 must be in [72/d0, 360/d0]
+    d_test[0,7] = 0.67/d_test[0,6] # d7 must be in [.25/d6, 1/d6]
     
     pbounds = { 'd0': (lb[0], ub[0]),
                 'd1': (lb[1], ub[1]),
@@ -83,15 +83,14 @@ if __name__ == '__main__':
         f=lambda d0, d1, d2, d3, d4, d5, d6, d7: 
             bo_friendly_objective(d0, d1, d2, d3, d4, d5, d6, d7, n_in, n_out, rel_std, clusters_inds_npz, corrs_clustered_npz),
         constraint=constraint,
+        acquisition_function=acq,
         pbounds=pbounds,
-        verbose=verbose, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
         random_state=random_state,
         )
         
     optimizer.maximize(
         init_points=init_points,
         n_iter=n_iter,
-        acquisition=acq
         )
     
     # NOTE: eig_mp expects d in shape [1,8] as an np.array or tensor

OED/utils_oed.py
@@ -15,6 +15,7 @@ import g as g_func
 from numba import njit, jit
 import time
 import multiprocessing
+import math
 
 def sample_prior(n_samps, seed=42):
     # parameters are, in order:
@@ -141,7 +142,7 @@ def logpdf_np_chol(x, mean, L):
     return log_pdf
 
 @njit(fastmath=True)
-def eval_log_likelihood_cov_numba_prime(y, g_eval, L, jitter=1e-4):
+def eval_log_likelihood_cov_numba_prime(y, g_eval, L):
     
     # compute g and eps, via y = g(theta, d) * (1 + eps)
     eps = (y - g_eval)/g_eval 
@@ -159,13 +160,27 @@ def utility_with_reuse_worker_cov(i, y_vals, model_evals, n_in, L):
     # Inner loop: process all j for a given i
     for j in range(n_in):
         log_likelihood = eval_log_likelihood_cov_numba_prime(y_vals, model_evals[j, :], L)
-        evidence += np.exp(log_likelihood)
+        g_prod = safe_prod(model_evals[j, :])
+        evidence += np.exp(log_likelihood)/g_prod
         if i == j:
             log_likelihood_ij_same = log_likelihood
+            g_prod_ij_same = g_prod
 
     evidence /= n_in
-    utility = log_likelihood_ij_same - np.log(evidence)
+    utility = log_likelihood_ij_same - np.log(g_prod_ij_same) - np.log(evidence)
+    #print(i, log_likelihood_ij_same, np.log(evidence), utility)
     return utility
+
+@njit(fastmath=True)
+def safe_prod(vector):  
+    # we can ignore the sign bc we want det(diag(vector))
+    # Take the logarithm of the vector
+    log_vector = np.log(np.abs(vector))
+    
+    # Compute the log-product of the vector (sum of logs)
+    log_product = np.sum(log_vector)
+    
+    return math.exp(log_product)
 
 # Outer function with multiprocessing for the outer loop
 def utility_with_reuse_mp_cov(y_vals, model_evals, n_in, n_out, cov_all, n_workers=None):
@@ -646,8 +661,8 @@ def utility_with_reuse_mp(y_vals, model_evals, n_in, n_out, rel_std, clusters_in
     all_inds = np.load("all_inds.npy")
 
     # Create a pool of workers
-    n_workers = n_workers or multiprocessing.cpu_count()-2
-    print("Number of cores in use: "+str(n_workers))
+    n_workers = n_workers or multiprocessing.cpu_count()-1
+    # print("Number of cores in use: "+str(n_workers))
     
     # for debugging only
     # results2 = []
@@ -715,8 +730,8 @@ def eig_mp(d, n_in, n_out, rel_std, clusters_inds_npz, corrs_clustered_npz, jitt
     clusters_inds_dict = {key: clusters_inds_npz[key] for key in clusters_inds_npz.files}
     corrs_clustered_dict = {key: corrs_clustered_npz[key] for key in corrs_clustered_npz.files}
 
-    print("starting sampling epsilon loop")
-    start_time = time.time()
+    # print("starting sampling epsilon loop")
+    # start_time = time.time()
     # for i in range(n_in):
     #     eps, _ = sample_epsilon_dict(g_evals[i,:], rel_std, clusters_inds_npz, corrs_clustered_npz, jitter)
     #     y_vals[i,:] = g_evals[i,:] + eps
@@ -744,15 +759,15 @@ def eig_mp(d, n_in, n_out, rel_std, clusters_inds_npz, corrs_clustered_npz, jitt
     #     eps, eps_cov = sample_epsilon_dict(g_evals[i,:], rel_std, clusters_inds_dict, corrs_clustered_dict, jitter)
     #     y_vals[i,:] = g_evals[i,:] + eps
     
-    stop_time = time.time()
-    dur = stop_time-start_time
-    print("dur for sample eps: " +str(dur))
+    # stop_time = time.time()
+    # dur = stop_time-start_time
+    # print("dur for sample eps: " +str(dur))
     
-    start_time = time.time()
+    # start_time = time.time()
     eig = utility_with_reuse_mp(y_vals, g_evals, n_in, n_out, rel_std, clusters_inds_npz, corrs_clustered_npz)
-    stop_time = time.time()
-    dur = stop_time-start_time
-    print("dur for eig " +str(dur))
+    # stop_time = time.time()
+    # dur = stop_time-start_time
+    # print("dur for eig " +str(dur))
     
     return eig
 